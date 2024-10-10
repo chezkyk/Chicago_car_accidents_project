b8a7b1276f6c6169ba9d9f1ba46704ff5407451b
chezkyk <Y0548532123@GMAIL.COM>
2024-10-10 18:02:09 +0300

    I added tests to check for cases where the input is not good.

--- tests/accident_test.py
+++ tests/accident_test.py
@@ -21,11 +21,23 @@ def test_get_accidents_by_area(client):
     response = client.get('/api/accident/get_accidents_by_area/225')
     assert response.status_code == 200
     assert 'total_accidents' in response.json
+    
+    
+def test_get_accidents_by_area_with_missing_parameter(client):
+    # parameter beat is missing
+    response = client.get('/api/accident/get_accidents_by_area')
+    assert response.status_code == 404
+
 
 def test_get_accidents_by_area_and_date(client):
     response = client.get('/api/accident/get_accidents_by_area_and_date?beat=123&time_period=day&start_date=2020-10-10&end_date=2024-10-10')
     assert response.status_code == 200
     assert 'total_accidents' in response.json
+    
+def test_get_accidents_by_area_and_date_with_missing_parameter(client):
+    # parameter beat is missing
+    response = client.get('/api/accident/get_accidents_by_area_and_date?beat=123&start_date=2020-10-10&end_date=2024-10-10')
+    assert response.status_code == 404
 
 def test_get_accidents_by_main_reason(client):
     response = client.get('/api/accident/get_accidents_by_main_reason/225')
@@ -34,6 +46,12 @@ def test_get_accidents_by_main_reason(client):
     assert 'area' in data
     assert 'causes' in data
     assert isinstance(data['causes'], dict)
+
+
+def test_get_accidents_by_main_reason_with_missing_parameter(client):
+    # parameter beat is missing
+    response = client.get('/api/accident/get_accidents_by_main_reason')
+    assert response.status_code == 404
 
 def test_get_injury_statistics(client):
     response = client.get('/api/accident/get_injury_statistics/225')
@@ -48,3 +66,8 @@ def test_get_injury_statistics(client):
         assert 'date' in first_entry
         assert 'injuries' in first_entry
         assert isinstance(first_entry['injuries'], dict)
+
+def test_get_injury_statistics_with_missing_parameter(client):
+    # parameter beat is missing
+    response = client.get('/api/accident/get_injury_statistics')
+    assert response.status_code == 404
