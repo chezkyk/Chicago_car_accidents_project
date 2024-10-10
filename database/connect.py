a3f710967f62ad23bd15c495881de558d08261ee
chezkyk <Y0548532123@GMAIL.COM>
2024-10-10 12:04:28 +0300

    added 3 collections, for month, week, day.

--- database/connect.py
+++ database/connect.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
+
 db = client['chicago_accidents']
-accident_collection = db['accidents']
+
+month_collection = db['monthly']
+week_collection = db['weekly']
+day_collection= db['daily']
