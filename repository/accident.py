6162bc95d43cf916ae70e0fdc8a7e56d06bacd92
chezkyk <Y0548532123@GMAIL.COM>
2024-10-10 15:47:40 +0300

    added get injury statistics from db function

--- repository/accident.py
+++ repository/accident.py
@@ -4,10 +4,17 @@ def fetch_total_accidents_from_db(beat):
     total_accidents = day_collection.count_documents({'area': beat})
     return total_accidents
 
+
 def accidents_by_area_and_date_from_db(query,collection):
     total_accidents = collection.count_documents(query)
     return total_accidents
 
+
 def accidents_by_main_reason_from_db(pipeline):
     results = list(day_collection.aggregate(pipeline))
     return results
+
+
+def get_injury_statistics_from_db(pipeline):
+    result = list(day_collection.aggregate(pipeline))
+    return result
