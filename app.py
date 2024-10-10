91c9a6898cc51564e7f51e7e95112a09f856ff05
chezkyk <Y0548532123@GMAIL.COM>
2024-10-10 17:12:40 +0300

    added a app register for accident bluprint to app file

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
+from blueprints.accident import accident_bp
 app = Flask(__name__)
 
+app.register_blueprint(accident_bp)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
