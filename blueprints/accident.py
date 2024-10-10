c44b91270f07c6d2b9216ff566efaceee93d1c35
chezkyk <Y0548532123@GMAIL.COM>
2024-10-10 12:02:46 +0300

    added a get accidents by area function

--- blueprints/accident.py
+++ blueprints/accident.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint,request
 from repository.csv_repoaitory import init_db
+from repository.accident import fetch_total_accidents_from_db
+from database.connect import month_collection,week_collection,day_collection
+
 accident_bp = Blueprint('accident', __name__,url_prefix='/api/accident')
 
 @accident_bp.route('/init_db', methods=['POST'])
@@ -9,7 +12,14 @@ def start_db():
 
 @accident_bp.route('/get_accidents_by_area', methods=['GET'])
 def get_accidents_by_area():
-    pass
+    beat = request.args.get('beat')
+
+    if not beat:
+        return {'error': 'beat (area) was NONE'}, 400
+
+    total_accidents = fetch_total_accidents_from_db(beat)
+
+    return {'total_accidents': total_accidents}, 200
 
 @accident_bp.route('/get_accidents_by_area_and_date', methods=['GET'])
 def get_accidents_by_area_and_date():
