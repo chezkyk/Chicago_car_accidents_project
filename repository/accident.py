91d27540ed935557d8c8d4e77d52cab0b4543622
chezkyk <Y0548532123@GMAIL.COM>
2024-10-10 15:27:50 +0300

    added accidents by main reason from db function

--- repository/accident.py
+++ repository/accident.py
@@ -1,9 +1,13 @@
 from database.connect import day_collection
 
 def fetch_total_accidents_from_db(beat):
-    total_accidents = day_collection.count_documents({'area': str(beat)})
+    total_accidents = day_collection.count_documents({'area': beat})
     return total_accidents
 
 def accidents_by_area_and_date_from_db(query,collection):
     total_accidents = collection.count_documents(query)
     return total_accidents
+
+def accidents_by_main_reason_from_db(pipeline):
+    results = list(day_collection.aggregate(pipeline))
+    return results
