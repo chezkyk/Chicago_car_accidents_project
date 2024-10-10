c2f2340a3fdb851f68f4ec39e506c8d7e860524c
chezkyk <Y0548532123@GMAIL.COM>
2024-10-10 14:41:06 +0300

    added accidents by area and date from_db function

--- repository/accident.py
+++ repository/accident.py
@@ -1,5 +1,9 @@
 from database.connect import day_collection
 
 def fetch_total_accidents_from_db(beat):
-    total_accidents = day_collection.count_documents({'area': beat})
+    total_accidents = day_collection.count_documents({'area': str(beat)})
+    return total_accidents
+
+def accidents_by_area_and_date_from_db(query,collection):
+    total_accidents = collection.count_documents(query)
     return total_accidents
